d9331d4eb96fe23673a423f0c1c02aab669a5f1a
Danill13and <114337660+Danill13and@users.noreply.github.com>
2024-11-04 11:33:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
-const { token } = require('./config.json');
+const token = process.env.token;
 const fs = require('fs');
 const path = require('node:path'); 
 
